e85fe584f892200ed4e7ca541ea669492ad7e9a4
Denis Vasilev <vassdeniss@gmail.com>
2023-12-14 01:07:40 +0200

    Added custom config file

---
+++ EmuPath.cs
@@ -0,0 +1,29 @@
+﻿namespace TransferMySaves;
+
+public class EmuPath
+{
+    public string DsPath { get; set; } = null!;
+
+    public string PspPath { get; set; } = null!;
+
+    public string? DsConverter { get; set; }
+
+    public string? PspConverter { get; set; }
+
+    public void DetermineFromToPaths(string selectedSource, out string fromPath, out string toPath)
+    {
+        fromPath = selectedSource switch
+        {
+            "3DS" => this.DsPath,
+            "PSP" => this.PspPath,
+            _ => throw new ArgumentOutOfRangeException(nameof(selectedSource), selectedSource, "Source does not exist!")
+        };
+
+        toPath = selectedSource switch
+        {
+            "3DS" => this.PspPath,
+            "PSP" => this.DsPath,
+            _ => throw new ArgumentOutOfRangeException(nameof(selectedSource), selectedSource, "Source does not exist!")
+        };
+    }
+}
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+
+using System.Reflection;
 
 namespace TransferMySaves;
+
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-        var builder = MauiApp.CreateBuilder();
+        MauiAppBuilder builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
@@ -14,9 +17,18 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
+        builder.Services.AddTransient<MainPage>();
+    
+
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        using Stream stream = assembly.GetManifestResourceStream("TransferMySaves.config.json")
+                              ?? throw new InvalidOperationException();
+
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonStream(stream)
+            .Build();
+
+        builder.Configuration.AddConfiguration(config);
 
         return builder.Build();
     }

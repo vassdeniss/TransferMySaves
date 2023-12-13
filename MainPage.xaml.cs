2a1ce1eca106b1fbcc6daf21826def50421c4eb3
Denis Vasilev <vassdeniss@gmail.com>
2023-12-13 11:07:32 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using FluentFTP;
+
+namespace TransferMySaves;
+
+public partial class MainPage : ContentPage
+{
+    private readonly string _ftpHost = "192.168.0.23";
+    private readonly int _ftpPort = 5000;
+
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private async void Button_OnClicked(object? sender, EventArgs e)
+    {
+        CancellationToken token = new();
+
+        using AsyncFtpClient ftp = new(this._ftpHost, this._ftpPort);
+
+        await ftp.Connect(token);
+
+        await ftp.DownloadFile(
+            @"C:\Users\vassd\Desktop\Pokemon - Emerald Version (USA, Europe).srm",
+            "/retroarch/cores/savefiles/gpSP/Pokemon - Emerald Version (USA, Europe).srm",
+            FtpLocalExists.Overwrite,
+            FtpVerify.Retry, token: token);
+    }
+}
+
